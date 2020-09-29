eb2fb53d3f029546161e81d7e31754b845f27c7b
filon13p <filon13p@gmail.com>
2020-09-29 14:25:46 +0300

    сделал кусок шестой

--- lab4-5/Lab4/RentService.cs
+++ lab4-5/Lab4/RentService.cs
@@ -1,31 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Lab4
 {
+
+
     ///<summary>
     ///Сервис проката  
     ///</summary>
     public class RentService
     {
+        /// <summary>
+        /// Единственный экземпляр класса RentService
+        /// </summary>
+        private static RentService _instance;
+
+
         ///<summery>
         /// Словарь клиентов
         ///</summery
-        public static Dictionary<int, Client>
-        Clients { get; } = new Dictionary<int, Client>();
+        private Dictionary<int, Client> _clients  = new Dictionary<int, Client>();
         ///<summary>
         ///Словарь автомобилей
         ///</summary> 
-        public static Dictionary<int, Car>
-        Cars { get; } = new Dictionary<int, Car>();
+        private Dictionary<int, Car> _cars = new Dictionary<int, Car>();
         ///<summary>
         ///Список выданных автомобилей
         ///</summary>
-        public static List<RentedCar> RentedCars
+       private  List<RentedCar> _rentedCars = new List<RentedCar>();
+        public IEnumerable<Client> Clients
         {
-            get;
-        } = new List<RentedCar>();
+            get { return _clients.Values.AsEnumerable(); }
+        }
+        /// <summary>
+        /// Коллекция номеров
+        /// </summary>
+        public IEnumerable<Car> Cars
+        {
+            get
+            {
+                return _cars.Values.AsEnumerable(); ;
+            }
+        }
+        /// <summary>
+        /// Коллекция поселений
+        /// </summary>
+        public IEnumerable<RentedCar> RentedCars
+        {
+            get
+            {
+                return _rentedCars;
+            }
+        }
+
+
+        public static RentService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new RentService();
+                }
+                return _instance;
+            }
+        }
+        private RentService()
+        {
+
+        }
 
     }
 }
