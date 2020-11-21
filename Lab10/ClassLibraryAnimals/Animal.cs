97a5961d06b8849d40906bda0a814a0d2fb770c7
filon13p <filon13p@gmail.com>
2020-11-21 18:52:31 +0300

    Начал 10. Сделал либу

---
+++ Lab10/ClassLibraryAnimals/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibraryAnimals
+{
+    /// <summary>
+    /// Животное
+    /// </summary>
+    public class Animal
+    {
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Латинское название
+        /// </summary>
+        public string Latin_Title { get; set; }
+        /// <summary>
+        /// Ареал обитания
+        /// </summary>
+        public string  Habitat { get; set; }
+        /// <summary>
+        /// Охранный статус
+        /// </summary>
+        public string Зrotection_Status { get; set; }
+        
+    }
+}
