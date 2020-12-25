bd719d87905596ff49025d50ea97ca05c6184194
filon13p <filon13p@gmail.com>
2020-12-25 18:41:52 +0300

    *починил ajax

--- Lab15/Lab15/Scripts/producers.js
+++ Lab15/Lab15/Scripts/producers.js
@@ -1,81 +1,95 @@
-﻿var apiBaseUrl = "http://localhost:2313/api/";
-var producerssUrl = apiBaseUrl + "producers/";
-function reloadproducerList() {
-	var producersList = $("#producersList");
+﻿var apiBaseUrl = "https://localhost:44334/api/";
+var producersUrl = apiBaseUrl + "Producers/";
+
+function reloadProducerList() {
+	var producersList = $("#ProducersList");
 	if (producersList.length) { //Есть элемент для списка пользователей - значит мы на главной странице - можно загрузить список
 		$.ajax({
-			url: producerssUrl,
+			url: producersUrl,
 			dataType: "json",
 			data: null,
 			type: "GET",
+		
 			success: function (data) {
+
+		
 				// Do something with the result
 				var itemsCount = data.length;
 				producersList.html("");
 				var html = "<table>";
 				for (var i = 0; i < itemsCount; i++) {
 					var item = data[i];
-					var id = item.id;
-					var firstName = item.FirstName;
-					var secondName = item.SecondName;
-					var lastName = item.LastName;
-					html += "<tr><td>" + lastName + "</td><td>" + firstName + "</td><td>" + secondName + "</td>";
-					html += "<td><a href='edit.html?id=" + id + "'>Редактировать</a>&nbsp;<a href='#' class='delete_producer' data-id='" + id + "'>Удалить</a></td></tr>";
+					var userId = item.id;
+					var fullName = item.FullName;
+				
+					html += "<tr><td>" +  fullName + "</td>";
+					html += "<td><a href='edit.html?id=" + userId + "'>Редактировать</a>&nbsp;<a href='#' class='delete_user' data-id='" + userId + "'>Удалить</a></td></tr>";
 				}
 				html += "</table>";
 				producersList.html(html);
+
+			},
+			error: function (xmlHttpRequest, textStatus, errorThrown) {
+				if (xmlHttpRequest.readyState == 0 || xmlHttpRequest.status == 0)
+					return;  // it's not really an error
+				else
+					alert("get failed");
 			}
 		});
+
 	}
+	
 }
-$(function () {
-	reloadproducerList();
-	var producerInfo = $("#producerInfo");
-	if (producerInfo.length) { //Есть элемент для информации о пользователе -  загрузить информацию о нём
+$(document).ready(function () {
+	reloadProducerList();
+	var producersList = $("#ProducersList");
+	if (producersList.length) { //Есть элемент для информации о пользователе -  загрузить информацию о нём
 		var url_string = window.location.href;
 		var url = new URL(url_string);
 		var id = url.searchParams.get("id");
 		if (id != null) {
 			$.ajax({
-				url: producersUrl + id + "/",
+				url: usersUrl + id + "/",
 				dataType: "json",
 				data: null,
 				type: "GET",
 				success: function (data) {
-					var id = data.id;
+					var userId = data.id;
 					var firstName = data.FirstName;
-				
 					var lastName = data.LastName;
 					$("#LastName").val(lastName);
 					$("#FirstName").val(firstName);
-			
+
 				}
 			});
-			producerInfo.after("<input type='button' id='EditProducer' value='Сохранить'/>");
+			producersList.after("<input type='button' id='EditProducer' value='Сохранить'/>");
 		}
 		else {
-			producerInfo.after("<input type='button' id='CreateProducer' value='Создать'/>");
+			producersList.after("<input type='button' id='CreateProducer' value='Создать'/>");
 		}
 	};
+
+
 	$(document).on("click", "a.delete_producer", function () {
 		var id = $(this).attr("data-id");
 		if (id != null) {
 			$.ajax({
-				url: producersUrl + id + "/",
+				url: usersUrl + id + "/",
 				dataType: "json",
 				data: null,
 				type: "DELETE",
 				success: function (data) {
-					reloadproducerList();
+					reloadUserList();
 				}
 			});
 		}
 	});
+
 	$(document).on("click", "#CreateProducer", function () {
 		var producerData = {
 			LastName: $("#LastName").val(),
 			FirstName: $("#FirstName").val(),
-		
+			
 		};
 		$.ajax({
 			url: producersUrl,
@@ -83,28 +97,29 @@ $(function () {
 			data: producerData,
 			type: "POST",
 			success: function () {
-				window.location.href = "../producers/index.html";
+				window.location.href = "../Producers/index.html";
 			}
 		});
 	});
+
 	$(document).on("click", "#EditProducer", function () {
 		var url_string = window.location.href;
 		var url = new URL(url_string);
 		var id = url.searchParams.get("id");
 		if (id != null) {
-			var producerData = {
-				id: id,
+			var userData = {
+				UserId: id,
 				LastName: $("#LastName").val(),
 				FirstName: $("#FirstName").val(),
-				
+			
 			};
 			$.ajax({
-				url: producerssUrl + id + "/",
+				url: producersUrl + id + "/",
 				dataType: "json",
-				data: producerData,
+				data: userData,
 				type: "PUT",
 				success: function () {
-					window.location.href = "../producers/index.html";
+					window.location.href = "../Producers/index.html";
 
 				}
 			});
